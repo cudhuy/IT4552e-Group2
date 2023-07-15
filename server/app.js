e9c3159dce78df8e0d09111e7127d34d2e32841b
Tran Huy <tdhuy141vt@gmail.com>
2023-07-16 03:20:01 +0700

    adding server and route

--- server/app.js
+++ server/app.js
@@ -16,12 +16,18 @@ app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 
+app.use('/api/admin', adminRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/menu', menuRoute);
+app.use('/api/cart', cartRoute);
+app.use('/api/order', orderRoute);
+app.use('/api/book', bookRoute);
 
 app.all('*', (req, res, next) => {
 	const err = new Error('The route can be not found');
 	err.statusCode = 404;
 	next(err);
-});
+}); // Route Error(1)
 
 app.use(errorHandler);
 
